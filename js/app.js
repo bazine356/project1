4b389dfa3d84bb5a8f1a058bb9b2fc876e138ee1
AZC <albertzcheng@gmail.com>
2020-11-07 11:36:58 -0800

    pseudo code and connecting index.html to javascript

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ var requestOptions = {
   redirect: 'follow'
 };
 
+//On clicking on submit button
+//Get data from Name input
+//Get data from Movie and Year input to generate data set from Omidb
+//Take out the data for Actors and call on love calculator on each Actor and compare to name
+//Output actor name and percentage
+
+
+
 fetch("https://love-calculator.p.rapidapi.com/getPercentage?fname=John&sname=Alice", {
   "method": "GET",
   "headers": {

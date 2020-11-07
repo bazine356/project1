aae15e6acdbe7a5d8ec00a35f5dbea95aa479c3e
Kieran Brobst <kieranbrobst@gmail.com>
2020-11-07 11:05:04 -0800

    added Bulma and basic elements

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@ var requestOptions = {
     redirect: 'follow'
   };
    
-  fetch('https://imdb-api.com/en/API/SearchSeries/k_82t1jm1h/saturday%20night%20live', requestOptions)
+  fetch('https://imdb-api.com/en/API/SearchName/k_82t1jm1h/Natalie%20Portman', requestOptions)
     .then(response => response.json())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
   
+    
